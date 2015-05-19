aeb2551b93a6e199086946c480d4398db473d646
Alex Uralov <5256301+uralov@users.noreply.github.com>
2015-05-19 21:41:54 +0300

    add decorator with parameters

--- decorators.py
+++ decorators.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+import time
 
 
 def my_decorator(fn):
-    print u"я декоратор с параметром %s"
+    print u"я декоратор"
 
     def wrapper(q):
         print u"я обёртка с параметром %s" % q
@@ -15,7 +16,33 @@ def my_decorator(fn):
 def my_func(q=1):
     print u"я функция с параметром %s" % q
 
+my_func(2)
+
 # decorated_func = my_decorator(my_func)
 # decorated_func(q=2)
 
-my_func(2)
+
+def sleep_decorator(seconds):
+    print "decorator with parameters"
+
+    def decorator(fn):
+        print "decorator body"
+
+        def wrapper(*args):
+            print "wrapper body"
+            time.sleep(seconds)
+            return fn(*args)
+
+        return wrapper
+
+    return decorator
+
+
+@sleep_decorator(2)
+def my_func2(q=1):
+    print u"my_func2 with param %s" % q
+
+my_func2(2)
+
+# decorated_func = sleep_decorator(2)(my_func2)
+# decorated_func(q=2)
