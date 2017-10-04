7529e4a0de9553565ddc7460f06bb220b931fee4
Alex Uralov <5256301+uralov@users.noreply.github.com>
2017-10-04 15:03:55 +0300

    refactored exchanger

--- exchanger.py
+++ exchanger.py
@@ -10,44 +10,51 @@ import json
 from urllib import request
 from urllib.error import URLError, HTTPError
 
-EXCHAGE_URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
+EXCHANGE_URL = 'https://query.yahooapis.com/v1/public/yql?q=select+*+from' \
+               '+yahoo.finance.xchange+where+pair+=+%22USDRUB%22&format=json' \
+               '&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
 
 
 class ExchangeException(Exception):
-	pass
-	
+    pass
+    
 
 def get_usd_amount_from_user():
-	usd_amount = ''
-	while not usd_amount.isdigit():
-		usd_amount = input('Введите сумму в долларах: ')
-	
-	return int(usd_amount)
+    amount = ''
+    while not isinstance(amount, float):
+        amount = input('Введите сумму в долларах: ')
+        try:
+            amount = float(amount)
+        except ValueError:
+            print('Значение должно быть челочисленным или разделённым точкой')
+    
+    return amount
 
 
 def get_usd_exchange_rate():
-	try:
-		responce = request.urlopen(EXCHAGE_URL)
-	except (URLError, HTTPError):
-		raise ExchangeException('Сервис {0} недоступен'.format(EXCHAGE_URL))
+    try:
+        response = request.urlopen(EXCHANGE_URL)
+    except (URLError, HTTPError):
+        raise ExchangeException('Сервис {0} недоступен'.format(EXCHANGE_URL))
 
-	exchanges_rate = json.loads(responce.read())
-	try:
-		usd_exchange_rate = exchanges_rate['Valute']['USD']['Value']
-	except KeyError:
-		raise ExchangeException('Не удалось получить курс USD') 
+    response = response.read().decode(response.headers.get_content_charset())
+    exchanges_rate = json.loads(response)
+    try:
+        exchange_rate = exchanges_rate['query']['results']['rate']['Rate']
+    except KeyError:
+        raise ExchangeException('Не удалось получить курс USD') 
 
-	return usd_exchange_rate
+    return float(exchange_rate)
 
 
 if __name__ == '__main__':
-	usd_amount = get_usd_amount_from_user()
-	try:
-		usd_exchange_rate = get_usd_exchange_rate()
-	except ExchangeException as e:
-		exit(e)
+    usd_amount = get_usd_amount_from_user()
+    try:
+        usd_exchange_rate = get_usd_exchange_rate()
+    except ExchangeException as e:
+        exit(e)
 
-	rub_amount = round(usd_amount * usd_exchange_rate, 2)
+    rub_amount = round(usd_amount * usd_exchange_rate, 2)
 
-	print('Текущий курс USD: {0}'.format(usd_exchange_rate))
-	print('{0} USD ~ {1} рублей'.format(usd_amount, rub_amount))
+    print('Текущий курс USD: {0}'.format(usd_exchange_rate))
+    print('{0} USD ~ {1} рублей'.format(usd_amount, rub_amount))
