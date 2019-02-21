a010883a32d9f310c63beb1a3d4fd02335797adb
Alexander Uralov <yralov87@gmail.com>
2019-02-21 15:52:25 +0300

    bubble sorting

--- patterns/singleton.py
+++ patterns/singleton.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
-
-
-class Singleton(object):
+class Singleton:
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
             cls.instance = super(Singleton, cls).__new__(cls)
         return cls.instance
 
+
 a = Singleton()
 b = Singleton()
-
-print a is b
+print(a is b)
